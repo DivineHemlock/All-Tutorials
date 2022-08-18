255312d0a3ddc07c7712145cf16ad53a3cbff362
mohsenBagherzadehAnsari <divinehemlock@gmail.com>
2022-08-18 09:19:05 +0430

    setup

---
+++ Spring Security PT.4/src/main/java/com/example/springsecuritypt4/SpringSecurityPt4Application.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritypt4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPt4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityPt4Application.class, args);
+    }
+
+}
---
+++ Spring Security PT.4/src/test/java/com/example/springsecuritypt4/SpringSecurityPt4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritypt4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityPt4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
