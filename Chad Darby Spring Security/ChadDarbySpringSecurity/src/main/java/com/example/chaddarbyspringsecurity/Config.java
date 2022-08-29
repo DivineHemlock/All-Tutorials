1d83646e4ca21971d8c675af73026dae35ec4da6
mohsenBagherzadehAnsari <divinehemlock@gmail.com>
2022-08-29 16:25:34 +0430

    chadDarby security done

--- Chad Darby Spring Security/ChadDarbySpringSecurity/src/main/java/com/example/chaddarbyspringsecurity/Config.java
+++ Chad Darby Spring Security/ChadDarbySpringSecurity/src/main/java/com/example/chaddarbyspringsecurity/Config.java
@@ -1,31 +1,60 @@
 package com.example.chaddarbyspringsecurity;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
+@PropertySource("classpath:application.properties")
 public class Config extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
-        auth.inMemoryAuthentication()
-                .withUser(users.username("admin").password("1").roles("admin"))
-                .withUser(users.username("employee").password("1").roles("employee"))
-                .withUser(users.username("manager").password("1").roles("manager"))
-        ;
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public JdbcUserDetailsManager jdbcUserDetailsManager(){
+        return new JdbcUserDetailsManager(dataSource());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder () {
+        return NoOpPasswordEncoder.getInstance();
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests().anyRequest().authenticated().and().formLogin().permitAll();
+        http.authorizeHttpRequests()
+                // if we add this below line , all authenticated users will have access to all of the content
+                //.anyRequest().authenticated()
+                // if we add this below line , user access will be limited based on roles and url pattern
+                // /** means all subdirectories
+                .antMatchers("/test/**").hasAnyRole("manager" , "admin")
+                .and().formLogin().permitAll()
+                .and().logout().permitAll()
+        ;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(environment.getProperty("jdbc.url"));
+        dataSource.setUsername(environment.getProperty("jdbc.user"));
+        dataSource.setPassword(environment.getProperty("jdbc.password"));
+        return dataSource;
     }
 }
+ //452
 
 
-//406
--- Chad Darby Spring Security/ChadDarbySpringSecurity/src/main/java/com/example/chaddarbyspringsecurity/Controller.java
+++ Chad Darby Spring Security/ChadDarbySpringSecurity/src/main/java/com/example/chaddarbyspringsecurity/Controller.java
@@ -1,5 +1,6 @@
 package com.example.chaddarbyspringsecurity;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 
     @GetMapping("/test")
-    public String test() {
-        return "test";
+    public String test(Authentication authentication) {
+        return "user :" + authentication.getName()+ "\n" + "test " + authentication.getAuthorities();
     }
+
+
 }
